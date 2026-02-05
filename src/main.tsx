a1c61f9cc3899ef5422644737ce18425774bba1f
gvs-canva <gvs.canva@googlemail.com>
2026-02-05 19:28:50 +0530

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
+import PacificFarms from './pages/PacificFarms.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/pacific-farms" element={<PacificFarms />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/pages/PacificFarms.tsx
@@ -0,0 +1,14 @@
+function PacificFarms() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <iframe
+        src="https://app.startinfinity.com/b/5FUQ8PbybMB"
+        className="w-full h-screen border-0"
+        title="Pacific Farms Board"
+        allow="fullscreen"
+      />
+    </div>
+  );
+}
+
+export default PacificFarms;
